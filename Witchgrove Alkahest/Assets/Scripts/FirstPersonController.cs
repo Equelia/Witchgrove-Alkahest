b4bb0a5e9939644d34dedc809cf92bd8e68c4a9f
Faradj Faradjov <faraway51423@gmail.com>
2025-06-11 18:14:46 +0500

    Added: jump & landing shake

--- Witchgrove Alkahest/Assets/Scripts/FirstPersonController.cs
+++ Witchgrove Alkahest/Assets/Scripts/FirstPersonController.cs
@@ -16,24 +16,22 @@ public class FirstPersonController : MonoBehaviour
     [SerializeField] private float mouseSensitivity = 100f;
 
     [Header("Walk Head Bob Settings")]
-    [Tooltip("Head bob frequency when walking")]
-    [SerializeField] private float walkBobFrequency = 1.5f;
-    [Tooltip("Horizontal bob amplitude when walking")]
-    [SerializeField] private float walkBobHorizontalAmplitude = 0.05f;
-    [Tooltip("Vertical bob amplitude when walking")]
-    [SerializeField] private float walkBobVerticalAmplitude = 0.05f;
-    [Tooltip("Head bob smooth when walking")]
-    [SerializeField] private float walkBobSmooth = 5f;
+    [Tooltip("Bob frequency when walking"), SerializeField] private float walkBobFrequency = 1.5f;
+    [Tooltip("Horizontal bob amplitude when walking"), SerializeField] private float walkBobHorizontalAmplitude = 0.05f;
+    [Tooltip("Vertical bob amplitude when walking"), SerializeField] private float walkBobVerticalAmplitude = 0.05f;
+    [Tooltip("Bob smooth when walking"), SerializeField] private float walkBobSmooth = 5f;
 
     [Header("Sprint Head Bob Settings")]
-    [Tooltip("Head bob frequency when sprinting")]
-    [SerializeField] private float sprintBobFrequency = 3f;
-    [Tooltip("Horizontal bob amplitude when sprinting")]
-    [SerializeField] private float sprintBobHorizontalAmplitude = 0.1f;
-    [Tooltip("Vertical bob amplitude when sprinting")]
-    [SerializeField] private float sprintBobVerticalAmplitude = 0.1f;
-    [Tooltip("Head bob smooth when sprinting")]
-    [SerializeField] private float sprintBobSmooth = 2f;
+    [Tooltip("Bob frequency when sprinting"), SerializeField] private float sprintBobFrequency = 3f;
+    [Tooltip("Horizontal bob amplitude when sprinting"), SerializeField] private float sprintBobHorizontalAmplitude = 0.1f;
+    [Tooltip("Vertical bob amplitude when sprinting"), SerializeField] private float sprintBobVerticalAmplitude = 0.1f;
+    [Tooltip("Bob smooth when sprinting"), SerializeField] private float sprintBobSmooth = 2f;
+
+    [Header("Jump/Land Shake Settings")]
+    [Tooltip("Duration of shake when jumping"), SerializeField] private float jumpShakeDuration = 0.2f;
+    [Tooltip("Amplitude of shake when jumping"), SerializeField] private float jumpShakeAmplitude = 0.1f;
+    [Tooltip("Duration of shake when landing"), SerializeField] private float landShakeDuration = 0.3f;
+    [Tooltip("Amplitude of shake when landing"), SerializeField] private float landShakeAmplitude = 0.15f;
 
     private CharacterController controller;
     private Vector3 velocity;
@@ -42,39 +40,60 @@ public class FirstPersonController : MonoBehaviour
     private Vector3 cameraStartLocalPos;
     private float bobTimer;
 
+    private float shakeTimer;
+    private float shakeDuration;
+    private float shakeAmplitude;
+    private bool previousGrounded;
+    private bool jumpRequested;
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
         cameraStartLocalPos = playerCamera.localPosition;
+        previousGrounded = controller.isGrounded;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
     void Update()
     {
-        HandleMovement();
         HandleMouseLook();
         HandleHeadBob();
+
+        if (Input.GetButtonDown("Jump"))
+            jumpRequested = true;
+
+        if (shakeTimer > 0f)
+            shakeTimer -= Time.deltaTime;
     }
 
-    private void HandleMovement()
+    void FixedUpdate()
     {
         bool isGrounded = controller.isGrounded;
+
         if (isGrounded && velocity.y < 0f)
             velocity.y = -2f;
 
         Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-        if (input.sqrMagnitude > 1f) input.Normalize();
-
+        if (input.sqrMagnitude > 1f)
+            input.Normalize();
         float speed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed;
         Vector3 move = transform.TransformDirection(input) * speed;
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (jumpRequested && isGrounded)
+        {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            TriggerShake(jumpShakeDuration, jumpShakeAmplitude);
+        }
+        jumpRequested = false;
 
-        velocity.y += gravity * Time.deltaTime;
+        velocity.y += gravity * Time.fixedDeltaTime;
         Vector3 finalMove = move + Vector3.up * velocity.y;
-        controller.Move(finalMove * Time.deltaTime);
+        controller.Move(finalMove * Time.fixedDeltaTime);
+
+        if (!previousGrounded && isGrounded)
+            TriggerShake(landShakeDuration, landShakeAmplitude);
+        previousGrounded = isGrounded;
     }
 
     private void HandleMouseLook()
@@ -93,21 +112,20 @@ public class FirstPersonController : MonoBehaviour
     {
         Vector2 movementInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         float magnitude = movementInput.magnitude;
-
         bool isSprinting = Input.GetKey(KeyCode.LeftShift);
 
         float frequency = isSprinting ? sprintBobFrequency : walkBobFrequency;
-        float horizontalAmplitude = isSprinting ? sprintBobHorizontalAmplitude : walkBobHorizontalAmplitude;
-        float verticalAmplitude   = isSprinting ? sprintBobVerticalAmplitude   : walkBobVerticalAmplitude;
+        float hAmp = isSprinting ? sprintBobHorizontalAmplitude : walkBobHorizontalAmplitude;
+        float vAmp = isSprinting ? sprintBobVerticalAmplitude : walkBobVerticalAmplitude;
         float smooth = isSprinting ? sprintBobSmooth : walkBobSmooth;
 
         Vector3 targetPos = cameraStartLocalPos;
-        
+
         if (controller.isGrounded && magnitude > 0f)
         {
             bobTimer += Time.deltaTime * frequency * magnitude;
-            float xOffset = Mathf.Cos(bobTimer) * horizontalAmplitude * magnitude;
-            float yOffset = Mathf.Sin(bobTimer * 2f) * verticalAmplitude * magnitude;
+            float xOffset = Mathf.Cos(bobTimer) * hAmp * magnitude;
+            float yOffset = Mathf.Sin(bobTimer * 2f) * vAmp * magnitude;
             targetPos += new Vector3(xOffset, yOffset, 0f);
         }
         else
@@ -115,6 +133,24 @@ public class FirstPersonController : MonoBehaviour
             bobTimer = 0f;
         }
 
+        if (shakeTimer > 0f)
+        {
+            float elapsed = shakeDuration - shakeTimer;
+            float progress = Mathf.Clamp01(elapsed / shakeDuration);
+            float damper = 1f - progress;
+            float shakeFreq = Mathf.PI * 4f;
+            float xShake = Mathf.Sin(elapsed * shakeFreq) * shakeAmplitude * damper;
+            float yShake = Mathf.Sin(elapsed * shakeFreq * 1.5f) * shakeAmplitude * damper;
+            targetPos += new Vector3(xShake, yShake, 0f);
+        }
+
         playerCamera.localPosition = Vector3.Lerp(playerCamera.localPosition, targetPos, Time.deltaTime * smooth);
+    }
+
+    private void TriggerShake(float duration, float amplitude)
+    {
+        shakeDuration = duration;
+        shakeTimer = duration;
+        shakeAmplitude = amplitude;
     }
 }
